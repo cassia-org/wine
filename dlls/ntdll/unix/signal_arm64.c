6130c28665c8d9b6573e13a3c95813ffbf9627f1
Billy Laws <blaws05@gmail.com>
2023-06-30 17:52:57 +0100

    ntdll: Warn when attempting to set LDT on arm64
    We want to be aware if programs call this as it is unimplemented in the JIT.

--- dlls/ntdll/unix/signal_arm64.c
+++ dlls/ntdll/unix/signal_arm64.c
@@ -1497,6 +1497,8 @@ static void usr2_handler( int signal, siginfo_t *siginfo, void *sigcontext )
  */
 NTSTATUS get_thread_ldt_entry( HANDLE handle, void *data, ULONG len, ULONG *ret_len )
 {
+   FIXME( "unimplemented" );
+
     return STATUS_NOT_IMPLEMENTED;
 }
 
@@ -1507,6 +1509,7 @@ NTSTATUS get_thread_ldt_entry( HANDLE handle, void *data, ULONG len, ULONG *ret_
  */
 NTSTATUS WINAPI NtSetLdtEntries( ULONG sel1, LDT_ENTRY entry1, ULONG sel2, LDT_ENTRY entry2 )
 {
+    FIXME( "unimplemented" );
     return STATUS_NOT_IMPLEMENTED;
 }
 
